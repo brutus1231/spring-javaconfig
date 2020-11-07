67576d5157c7e9c8fe5751bf6fc69afa04313ea3
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-11-07 13:32:55 +0100

    hello world bean

--- src/main/java/pl/sda/ApplicationConfiguration.java
+++ src/main/java/pl/sda/ApplicationConfiguration.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("pl.sda")
 public class ApplicationConfiguration {
 
-/*    @Bean
-    public HelloWorld helloWorld() {
+    @Bean
+    public HelloWorld helloWorldBean() {
         return new HelloWorldImpl();
-    }*/
+    }
 }
